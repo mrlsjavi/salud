7f9c3324baa3fa08d604d968c76a0fb2ec49d706
Elioth Rivera <elioth010@gmail.com>
2016-09-26 16:24:34 -0600

    mte unidades de medida

---
+++ controllers/unidad_medida.php
@@ -0,0 +1,57 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+class Unidad_Medida extends Controller{
+
+	function __construct(){
+		parent::__construct(); //llamar el construct del padre que es controller(libs/controller)
+
+
+		//Auth::handleLogin();
+
+
+		//ya tenogo incluido el jquery y aqui mando a llamar su javascript independiente de cada vista
+		$this->view->js = array('unidad_medida/js/default.js');
+
+
+	}
+
+	function index(){
+		$this->view->title = 'Unidad de Medida';
+		$this->view->render('header');
+		//echo Hash::create('md5', 'test', HASH_PASSWORD_KEY);
+		//echo Hash::create('sha256', 'test', HASH_PASSWORD_KEY);
+		//vista carpeta/archivo
+		$this->view->render('unidad_medida/index');
+		$this->view->render('footer');
+	}
+
+
+
+	function guardar(){
+		$this->model->guardar();
+	}
+
+	function llenar_tabla(){
+		$this->model->llenar_tabla();
+	}
+
+	function eliminar(){
+		$this->model->eliminar();
+	}
+
+	function traer_usuarios(){
+		$this->model->traer_usuarios();
+	}
+
+	function traer_dato(){
+		$this->model->traer_dato();
+	}
+
+	function actualizar(){
+		$this->model->actualizar();
+	}
+
+
+
+
+}
