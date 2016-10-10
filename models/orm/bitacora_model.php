1c781ef50aefefc24489e197705ad58b410e2024
Elioth Rivera <elioth010@elioth010.elioth010>
2016-10-10 09:16:01 -0600

    cambios bitacora

--- models/orm/bitacora_model.php
+++ models/orm/bitacora_model.php
@@ -5,25 +5,41 @@ class Bitacora_Model {
 	public function __construct(){
 
 			//parent::__construct();
-			
+
 	}
 
 	public function guardar(){
-		$info =   json_decode(str_replace("\\", "", $_POST['info']));
-		print_r($info);
+		$info = json_decode(str_replace("\\", "", $_POST['info']));
+		$dispositivo = dispositivo_orm::where('ip', $info->ip);
+
+		$data = $info->data;
+		$datosArray = explode("|", $data);
+
+		//Sensor Oxigeno
+		$sensorOxigeno = sensor_orm::find(1);
+		//Sensor Oxigeno
+		$sensorTemperatura = sensor_orm::find(2);
+		//Sensor Aire
+		$sensorFlujoAire = sensor_orm::find(3);
+		//Sensor Aire
+		$sensorPresion = sensor_orm::find(4);
+
 		
+
+
+		$
 		//var_dump($info);
 		/*foreach ($info as $i) {
 			# code...
 			echo $i->sensor;
 		}*/
 
-		
-		
+
+
 	}
 
 
-	
+
 }
 
 ?>
